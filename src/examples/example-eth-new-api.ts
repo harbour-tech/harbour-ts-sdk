9ada97d8029f2ea5dbc80759792eac46e43e85ec
Martin <MartinBechtle@users.noreply.github.com>
2024-08-20 15:43:50 +0200

    wip

--- src/examples/example-eth-new-api.ts
+++ src/examples/example-eth-new-api.ts
@@ -1,7 +1,7 @@
 // In this example we demonstrate how an Ethereum wallet will generate the required signature for redirecting to the Harbour Ramp web app.
 // Run me with: npx tsx src/examples/example-eth-new-api.ts
 
-import {keccak256, toUtf8Bytes, Wallet} from "ethers";
+import {keccak256, Wallet} from "ethers";
 
 import {AuthClient} from "../";
 import {
@@ -31,8 +31,13 @@ const auth = new AuthClient("https://dev-api.harborapps-nonprod.link");
 // The first time a user connects their wallet and selects and address for on-ramping, they need to whitelist their crypto wallet address.
 // Since the endpoint is idempotent, you can repeat the action every time, without worrying about "is it already whitelisted" logic,
 // or you can implement such logic to optimize network usage, your choice.
-const pkPayload = hexToBinary(wallet.signingKey.publicKey);
-const signature = signPayload(pkPayload);
+const pkBytes = hexToBinary(wallet.signingKey.publicKey);
+const timestamp = new Date().getTime();
+const timestampBytes = new TextEncoder().encode(timestamp.toString());
+const payload = new Uint8Array(pkBytes.length + timestampBytes.length);
+payload.set(timestampBytes, 0);
+payload.set(pkBytes, timestampBytes.length);
+const signature = signPayload(payload);
 
 console.log("Sending whitelist request")
 console.log("Signature (hex): ", signature);
@@ -44,6 +49,7 @@ const whitelistResp = await auth.authenticateWallet(
         publicKeyType: AuthenticateWalletRequest_PublicKeyType.SECP256K1,
         hashingAlgo: AuthenticateWalletRequest_HashingAlgo.KECCAK256,
         signature: hexToBinary(signature),
+        timestamp: timestamp,
     }),
 );
 
@@ -52,7 +58,7 @@ console.dir(whitelistResp, {depth: null});
 
 function hexToBinary(hexString: string): Uint8Array {
     // Remove '0x' prefix if present
-    const hex = hexString.startsWith('0x') ? hexString.slice(2) : hexString;
+    const hex = hexString.toLowerCase().startsWith('0x') ? hexString.slice(2) : hexString;
 
     // Convert the hex string to a Uint8Array
     return new Uint8Array(hex.match(/.{1,2}/g)?.map(byte => parseInt(byte, 16)) || []);
